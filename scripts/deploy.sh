072241095bc9ca7e6260d931ebfcd4f502910e58
Martin Caarels <martin@caarels.com>
2017-10-16 13:58:40 +0200

    fixed route env var

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,6 @@
 cf login -a $api -u $username -p $password -o $organization -s $space &&\
 cd ./this-git-repo/webapp/ &&\
 cf push &&\
-cf map-route $route $domain --hostname rws-concourse-test &&\
+cf map-route concourse-app-staged $domain --hostname $route &&\
 cf delete concourse-app -f &&\
 cf rename concourse-app-staged concourse-app
