645853a5db4849321ba78101c1c070b15e6cbb81
Martin Caarels <martin@caarels.com>
2017-10-16 13:50:32 +0200

    fixed git uri

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,6 @@
 cf login -a $api -u $username -p $password -o $organization -s $space &&\
 cd ./this-git-repo/webapp/ &&\
 cf push &&\
-cf map-route concourse-app-staged cfapps.io --hostname rws-concourse-test &&\
+cf map-route $route $domain --hostname rws-concourse-test &&\
 cf delete concourse-app -f &&\
 cf rename concourse-app-staged concourse-app
