99c186964a375ba51a44592e1f16267e98745f88
Martin Caarels <martin@caarels.com>
2017-10-18 13:53:14 +0200

    scale loop

--- scripts/scale.sh
+++ scripts/scale.sh
@@ -1,5 +1,31 @@
 #!/bin/sh
-set
 
-cf login -a $api -u $username -p $password -o $organization -s $space &&\
-cf curl "/v2/apps" | jq .  
+# timeout is iterations times duration in seconds, so 100 * 3 = 300 seconds is 5 minutes.
+iterations="100"
+duration="3"
+
+
+
+
+cf login -a $api -u $username -p $password -o $organization -s $space
+
+index=$(cf curl "/v2/apps" | jq '[ .resources[].entity.name ]' | jq 'index("concourse-app")')
+guid=$(cf curl "/v2/apps" | jq ".resources[$index].metadata.guid" | tr -d '"')
+
+echo $guid
+
+while [ $i -gt 0 ]
+do
+  instances=$(cf curl "/v2/apps/$guid" | jq .entity.instances)
+  echo "Iteration: $iterations"
+  echo "Number of instances is: $instances"
+  if [ "$instances" = "1" ]
+  then
+    echo "App is scaled down, exiting."
+    exit 0
+  fi
+  sleep $duration
+  iterations=$[$iterations-1]
+done
+echo "----------> Timed-Out!"
+exit 1
